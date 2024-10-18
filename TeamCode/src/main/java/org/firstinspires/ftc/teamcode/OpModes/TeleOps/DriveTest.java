5cc0520f581358aa98655e72e6b3c125676f0b34
eitandas <142007178+eitandas@users.noreply.github.com>
2024-10-18 18:02:03 +0300

    added Drive test Opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/DriveTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOps;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.SubSystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.Utils.Poses.Vector2d;
+
+@TeleOp(name = "DriveTest", group = "TeleOps")
+public class DriveTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveTrain drive = new DriveTrain(this);
+
+        while (opModeIsActive()){
+            drive.FieldCentricDrive(new Vector2d(gamepad1.left_stick_x,-gamepad1.left_stick_y),gamepad1.right_stick_x);
+        }
+    }
+}
